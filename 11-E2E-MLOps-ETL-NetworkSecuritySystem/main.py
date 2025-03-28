d1a46e9265bcf198bf25816654279477ebc38312
anusha-chebolu <s.anusha.chebolu@gmail.com>
2025-03-28 16:00:41 -0400

    added model_trainer.py component

--- 11-E2E-MLOps-ETL-NetworkSecuritySystem/main.py
+++ 11-E2E-MLOps-ETL-NetworkSecuritySystem/main.py
@@ -3,9 +3,10 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
 import sys
 
 if __name__ == '__main__':
@@ -30,10 +31,16 @@ if __name__ == '__main__':
         data_transformation_config = DataTransformationConfig(training_pipeline_config)
         data_transformation = DataTransformation(data_validation_aritfact, data_transformation_config )
         logging.info("Initiate the data transformation")
-        data_transformation_aritfact = data_transformation.initiate_data_transformation()
-        print(data_transformation_aritfact)
+        data_transformation_artifact = data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
         logging.info("Data Transformation Completed")
 
+        logging.info("Model Training sstared")
+        model_trainer_config = ModelTrainerConfig(training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config = model_trainer_config,
+                                     data_transformation_artifact = data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
