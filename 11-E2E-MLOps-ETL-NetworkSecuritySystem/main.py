6867d8cb1875d82f97b19f2cd8a5d68b60986cdb
anusha-chebolu <s.anusha.chebolu@gmail.com>
2025-03-28 13:13:22 -0400

    added data_validation.py component

--- 11-E2E-MLOps-ETL-NetworkSecuritySystem/main.py
+++ 11-E2E-MLOps-ETL-NetworkSecuritySystem/main.py
@@ -1,17 +1,28 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
 if __name__ == '__main__':
     try:
-        trainingpipelineconfig = TrainingPipelineConfig()
-        dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
-        dataingestion = DataIngestion(dataingestionconfig)
+        training_pipeline_config = TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config)
+        data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("Initiate the data ingestion")
-        dataingestionaritfact = dataingestion.initiate_data_ingestion()
-        print(dataingestionaritfact)
+        data_ingestion_aritfact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(data_ingestion_aritfact)
+
+
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_aritfact, data_validation_config )
+        logging.info("Initiate the data validation")
+        data_validation_aritfact = data_validation.initiate_data_validation()
+        print(data_validation_aritfact)
+
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
