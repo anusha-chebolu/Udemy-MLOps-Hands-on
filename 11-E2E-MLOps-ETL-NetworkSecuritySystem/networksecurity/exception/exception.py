a5c55551e6c5aaa25e5c602bf9503c6c9a916833
anusha-chebolu <s.anusha.chebolu@gmail.com>
2025-03-28 11:42:06 -0400

    added data_ingestion.py component

--- 11-E2E-MLOps-ETL-NetworkSecuritySystem/networksecurity/exception/exception.py
+++ 11-E2E-MLOps-ETL-NetworkSecuritySystem/networksecurity/exception/exception.py
@@ -1,13 +1,27 @@
 import sys
+import inspect
 
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_details:sys):
+    def __init__(self, error_message, error_details: sys):
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
-        
-        self.lineno = exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.co_filename 
-    
+
+        try:
+            # Try to extract traceback info if inside an exception context
+            _, _, exc_tb = error_details.exc_info()
+            if exc_tb is not None:
+                self.lineno = exc_tb.tb_lineno
+                self.file_name = exc_tb.tb_frame.f_code.co_filename
+            else:
+                # Fallback if no exception context
+                frame = inspect.currentframe().f_back
+                self.lineno = frame.f_lineno
+                self.file_name = frame.f_code.co_filename
+        except:
+            self.lineno = -1
+            self.file_name = "Unknown"
+
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
+        return (
+            f"Error occured in python script name [{self.file_name}] "
+            f"line number [{self.lineno}] error message [{str(self.error_message)}]"
+        )
