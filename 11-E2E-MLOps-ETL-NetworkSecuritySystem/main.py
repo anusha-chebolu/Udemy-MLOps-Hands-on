f960b8a3e5feba323ad016b51057ec25b6062967
anusha-chebolu <s.anusha.chebolu@gmail.com>
2025-03-28 14:10:37 -0400

    added data_transformation.py component

--- 11-E2E-MLOps-ETL-NetworkSecuritySystem/main.py
+++ 11-E2E-MLOps-ETL-NetworkSecuritySystem/main.py
@@ -1,9 +1,11 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
 import sys
 
 if __name__ == '__main__':
@@ -22,6 +24,15 @@ if __name__ == '__main__':
         logging.info("Initiate the data validation")
         data_validation_aritfact = data_validation.initiate_data_validation()
         print(data_validation_aritfact)
+        logging.info("Data Validation Completed")
+
+
+        data_transformation_config = DataTransformationConfig(training_pipeline_config)
+        data_transformation = DataTransformation(data_validation_aritfact, data_transformation_config )
+        logging.info("Initiate the data transformation")
+        data_transformation_aritfact = data_transformation.initiate_data_transformation()
+        print(data_transformation_aritfact)
+        logging.info("Data Transformation Completed")
 
 
     except Exception as e:
